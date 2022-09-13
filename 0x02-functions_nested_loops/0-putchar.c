1d0815f4fcc1a567ebdf1b336653a969a1eec0dc
dessy53 <desxy53@gmail.com>
2022-09-13 16:37:33 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "des.h"
+
+/**
+  * main - print putchar
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
