c26045d787e425738648d14736992963ba5d90f0
dessy53 <desxy53@gmail.com>
2022-09-14 04:15:15 -0700

    Task 2 corrected

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,11 @@
 /**
   * print_alphabet_x10 - function to print abc 10 times
   * Return: 0
-  *
   */
-:%s/\s\+$//evoid print_alphabet_x10(void)
+
+void print_alphabet_x10(void)
 {
 char c, i;
-
 for (i = 0; i <= 9; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
