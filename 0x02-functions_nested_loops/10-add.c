6a3172962418874f9e3c9ef23f36f02da35e996f
dessy53 <desxy53@gmail.com>
2022-09-15 06:15:06 -0700

    Task 11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,7 @@
 /**
  * add - Adds two integars.
  * @num1: The first integar to be added.
- * @num2: The second integar to be added.
- * 
+ * @num2: The second integar to be added. 
  * Return: the result of the addition.
  */
 int add(int num1, int num2)
