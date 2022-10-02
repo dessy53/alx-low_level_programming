04c6f0b78c7cbd56374637815c22aba351406aed
dessy53 <desxy53@gmail.com>
2022-10-02 05:08:35 -0700

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Description: prints a string
+  * on success: returns no error
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
+
